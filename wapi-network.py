b37d9ae182e4cea5954062cfad75d0eaf729e488
Patrick Piper <patrick@ddiguru.com>
2021-05-12 10:38:04 -0600

    added netaddr requirement

--- wapi-network.py
+++ wapi-network.py
@@ -17,7 +17,9 @@ ibx_grid_master = '192.168.1.2'
 ibx_username = 'admin'
 ibx_password = 'infoblox'
 ibx_wapi_version = 'v2.11'
-
+ip_block = '10.0.0.0/8'
+cidr = 24
+num_of_networks = 1024
 log_level = logging.DEBUG
 log_format = '%(asctime)s %(levelname)s %(message)s'
 logger = logging.getLogger()
@@ -29,32 +31,19 @@ coloredlogs.install(
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-def get_nets(block='10.0.0.0/8', cidr=24, num_of_networks=1024):
-    base_network = IPNetwork(block)
+def get_nets():
+    base_network = IPNetwork(ip_block)
     subnets = list(base_network.subnet(cidr, count=num_of_networks))
     return subnets
 
 
-def get_args():
-    parser = argparse.ArgumentParser(description='script description')
-    parser.add_argument('-b', '--block', help='network block in CIDR form to build list from')
-    parser.add_argument('-c', '--cidr', type=int, help='size of networks to build (int)')
-    parser.add_argument('-n', '--number', type=int, help='number of consecutive subnets to create')
-    return parser.parse_args()
-
-
 def main():
-    args = get_args()
-    ip_block = args.block or '10.0.0.0/8'
-    size_of_networks = args.cidr or 24
-    num_of_networks = args.number or 1024
-
     url = f'https://{ibx_grid_master}/wapi/{ibx_wapi_version}'
     with requests.session() as s:
         grid = s.get(f'{url}/grid', auth=(ibx_username, ibx_password), verify=False)
         logger.info(grid.json())
 
-    networks = get_nets(ip_block, size_of_networks, num_of_networks)
+    networks = get_nets()
 
     t1_start = perf_counter()
 
