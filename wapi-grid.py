92143d3649014080a22786373a49387128b2adff
Patrick Piper <patrick@ddiguru.com>
2021-05-12 10:53:27 -0600

    simple refactor on the session object

--- wapi-grid.py
+++ wapi-grid.py
@@ -4,15 +4,16 @@ import requests
 from time import sleep
 import urllib3
 import logging
+
 logging.basicConfig(level=logging.DEBUG)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-s = requests.session()
-s.verify = False
-url = 'https://192.168.40.58/wapi/v2.11/grid'
+with requests.session() as s:
+    s.verify = False
+    url = 'https://192.168.40.58/wapi/v2.11/grid'
 
-s.get(url, auth=('admin', 'infoblox'), verify=False)
+    s.get(url, auth=('admin', 'infoblox'), verify=False)
 
-while True:
-    s.get(url, verify=False)
-    sleep(1)
+    while True:
+        s.get(url, verify=False)
+        sleep(1)
